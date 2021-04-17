d04c3208c4f777629eab418019248c71c998e7b9
xutai <514292663@qq.com>
2021-04-17 15:12:51 +0800

    Building a RESTful Web Service

--- src/test/java/com/example/restservice/GreetingControllerTests.java
+++ src/test/java/com/example/restservice/GreetingControllerTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.example.restservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class GreetingControllerTests {
 
 	@Test
 	void contextLoads() {
